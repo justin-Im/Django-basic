c106b94df7d4667c27af4a599d6b0603bbb43401
Justin <crush300@naver.com>
2021-05-26 00:02:20 +0900

    모델DB생성
    model 생성 후 아래 명령어 수행
    1. model init
    python manage.py makemigrations
    
    2. model db생성
    python manage.py migrate

--- fc_community/settings.py
+++ fc_community/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'board'
-    'fcuser'
+    'board',
+    'fcuser',
 ]
 
 MIDDLEWARE = [
