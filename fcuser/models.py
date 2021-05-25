9e86160de1c83abc6f7b0f97938d8f1797586846
Justin <crush300@naver.com>
2021-05-26 00:11:26 +0900

    [DB 생성 commands] model 생성 후 아래 명령어 수행 1. model init python manage.py makemigrations
    2. model db생성
    python manage.py migrate

--- fcuser/models.py
+++ fcuser/models.py
@@ -10,3 +10,10 @@ class Fcuser(models.Model):
                                             verbose_name='등록시간')
     class Meta:
         db_table = 'fastcampus_fcuser'
+
+#model 생성 후 아래 명령어 수행
+#1. model init
+#python manage.py makemigrations
+
+#2. model db생성
+#python manage.py migrate
