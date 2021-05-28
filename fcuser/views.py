e77f5d07893cd1eae0b08d7fb638697d122f117a
Justin <crush300@naver.com>
2021-05-29 00:38:29 +0900

    login - form활용

--- fcuser/views.py
+++ fcuser/views.py
@@ -2,12 +2,13 @@ from django.http import HttpResponse
 from django.shortcuts import render,redirect
 from django.contrib.auth.hashers import make_password, check_password
 from.models import Fcuser
+from .forms import LoginForm
 
 # Create your views here.
 # url로 접근했냐? register로 진입했냐?
 
 def home(request):
-    user_id = requset.session.get('user')
+    user_id = request.session.get('user')
 
     if user_id:
         fcuser = Fcuser.objects.get(pk=user_id)
@@ -23,24 +24,15 @@ def logout(request):
 
 
 def login(request):
-    if request.method == 'GET':
-        return render(request, 'login.html')
-    elif request.method == 'POST':
-        username = request.POST.get('username', None)
-        password = request.POST.get('password', None)
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            request.session['user'] = form.user_id
+            return redirect('/')
+    else:
+        form = LoginForm()
 
-        res_data = {}
-        if not (username and password):
-            res_data['error'] = '모든 값을 입력해야합니다.'
-        else:
-            fcuser = Fcuser.objects.get(username=username)
-            if check_password(password, fcuser.password):
-                request.session['user'] = fcuser.id
-                return redirect('/')
-            else:
-                res_data['error'] = '비밀번호를 틀렸습니다.'
-                           
-        return render(request, 'login.html', res_data)
+    return render(request, 'login.html', {'form': form})
 
 
 
