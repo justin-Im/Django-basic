7ca5cefec29b2ea775a1bb3270bc17e301e5913e
Justin <crush300@naver.com>
2021-06-02 01:00:24 +0900

    board - link buttom설정

--- fcuser/views.py
+++ fcuser/views.py
@@ -12,9 +12,8 @@ def home(request):
 
     if user_id:
         fcuser = Fcuser.objects.get(pk=user_id)
-        return HttpResponse(fcuser.username)
-
-    return HttpResponse('Home!')
+ 
+    return render(request, 'home.html')
 
 def logout(request):
     if request.session.get('user'):
